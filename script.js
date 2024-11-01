01fa0128a67af111afd92a59fda2216d8538df5f
lucas <lucas.shintani@gmail.com>
2024-11-01 08:45:32 -0400

    mudanca1

---
+++ script.js
@@ -0,0 +1,12 @@
+// codigo exemplo
+
+var pedro = "pacu"
+
+//  configuração de login
+//  --> git config --global user.nome "seu nome aqui"
+//  --> git config --global user.email "seu emai aqui"
+
+
+//  1 --> git init - inicializr o repositório
+//  2 --> git remote add origin "link do repositório aqui"
+//  3 --> git commmit -m "nome do commit"
